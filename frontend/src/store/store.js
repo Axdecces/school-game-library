715d52f7f663cd7e5c06b2d1bb6fb99a3248a251
Simon Wagner <Simon.Wagner@concat.de>
2021-10-05 12:50:08 +0200

    Added deleted games Slice

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import gamesReducer from '../features/games/gamesSlice';
+import deletedGamesReducer from '../features/games/deletedGamesSlice';
 import tagsReducer from '../features/tagsSlice';
 
 
 export const store = configureStore({
   reducer: {
     games: gamesReducer,
+    deletedGames: deletedGamesReducer,
     tags: tagsReducer
   },
 });
