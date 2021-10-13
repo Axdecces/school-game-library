8523807d930dc4c6f66b9f53466f1c5ef4ad9b93
Simon Wagner <axdecces@gmail.com>
2021-10-13 09:44:50 +0200

    Added FAB for adding new games, which doesn't do anything at the moment

--- frontend/src/features/games/GamesList.jsx
+++ frontend/src/features/games/GamesList.jsx
@@ -3,10 +3,16 @@ import { useSelector } from 'react-redux';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 import Game from './Game';
 
 import { selectAllGames } from "./gamesSlice";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+import './GamesList.scss'
 
 function GamesList() {
   const games = useSelector(selectAllGames);
@@ -15,7 +21,11 @@ function GamesList() {
       <Row xs={1} sm={2} lg={3} xxl={4} className="g-5 justify-content-evenly">
         {games && games.map(game => {return <Game key={game.id} id={game.id} />})}
       </Row>
+      <Button className='fab'>
+        <FontAwesomeIcon icon={faPlus} size="3x" className='icon' />
+      </Button>
     </Container>
+    
   )
 }
 
